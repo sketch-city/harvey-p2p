5941c75d50880b2b45a165cd9e015c66a6558762
amanda <pandafulmanda@gmail.com>
2017-09-02 17:23:38 -0500

    form submitting with types entering as list

--- public/js/main.js
+++ public/js/main.js
@@ -1,15 +1,14 @@
 $(document).ready( function() {
+  var expectedKeys = ['Text_Input', 'Phone', 'Zip', 'Offer_Type', 'Offer_Detail', 'Need_Type', 'Needs_Detail', 'Language', 'Name', 'Email'];
+
   // function to submit needs form
   $('form').submit(function(event) {
     event.preventDefault();
     var $form = $(this);
     var $submit = $form.find('[type="submit"]');
-    var formData = $form.serialize();
-    var formDataArray = $form.serializeArray();
+    var formData = getFormData($form);
     var $success = $form.siblings('.w-form-done');
     var $failure = $form.siblings('.w-form-fail');
-
-    // console.info(formData, formDataArray);
 
     var currentSubmitText = $submit.attr('value');
     $submit.attr('value', $submit.attr('data-wait'));
@@ -28,6 +27,27 @@ $(document).ready( function() {
         $submit.attr('value', currentSubmitText);
       });
   });
+
+  function getFormData($form){
+    var formDataArray = $form.serializeArray();
+    var formData = {};
+    Webflow._.each(formDataArray, function(entry){
+      if (entry.name.substr(-2) === '[]') {
+        entry.name = entry.name.substr(0, entry.name.length - 2);
+        formData[entry.name] = formData[entry.name] || '';
+        formData[entry.name] += (formData[entry.name] && ', ') + entry.value;
+        return;
+      }
+      if (expectedKeys.indexOf(entry.name) < 0) {
+        formData.Notes = formData.Notes || '';
+        formData.Notes += entry.name + ': ' + entry.value;
+        return;
+      }
+      formData[entry.name] = entry.value;
+    });
+
+    return formData;
+  }
 
 //   // change is-checked class on buttons
 //   $('.button-group').each(function( i, buttonGroup ) {

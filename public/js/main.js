9813ab351cc2c131014db9206a779c423572b4a7
amanda <pandafulmanda@gmail.com>
2017-09-02 16:51:44 -0500

    both forms submitting

--- public/js/main.js
+++ public/js/main.js
@@ -1,17 +1,31 @@
 $(document).ready( function() {
   // function to submit needs form
-  $('#wf-form-Need-Form').submit(function(event) {
+  $('form').submit(function(event) {
     event.preventDefault();
-    var formData = $(this).serialize();
-    $.post("/api/v1/web/needs", formData)
+    var $form = $(this);
+    var $submit = $form.find('[type="submit"]');
+    var formData = $form.serialize();
+    var formDataArray = $form.serializeArray();
+    var $success = $form.siblings('.w-form-done');
+    var $failure = $form.siblings('.w-form-fail');
+
+    // console.info(formData, formDataArray);
+
+    var currentSubmitText = $submit.attr('value');
+    $submit.attr('value', $submit.attr('data-wait'));
+    // return;
+    $.post($form.attr('action'), formData)
       .done(function(response) {
         // TODO: verify success
-        $('.form').hide();
-        $('.w-form-done').show();
+        $success.show();
       })
       .fail(function(error) {
-        $('.form').hide();
-        $('.w-form-fail').show();
+        $failure.show();
+      })
+      .always(function(){
+        $form.hide();
+        $form[0].reset();
+        $submit.attr('value', currentSubmitText);
       });
   });
 

861ef3232f536de58687e916173b015cc8b202f8
Will Clifton <wclifton1@gmail.com>
2017-09-04 16:29:57 -0500

    form, UI, SMS wording improvements

--- routes/api/v1/twilio.js
+++ routes/api/v1/twilio.js
@@ -14,23 +14,23 @@ const smsText = [
     trigger : "NEED",
     yes: "YES",
     messages : {
-      step1: "What do you need?",
+      step1: "Thank you for contacting Harvey Needs Matching. We are very sorry for the loss you have experienced. What are your immediate needs? Please be specific.",
       step2: handlebars.compile('Can we contact you at {{ phone }}? ' +
-        'Reply "{{ yes }}" or provide alternate number.'),
+        'Reply "{{ yes }}" or provide alternate contact info.'),
       step3: "What is your current zip code?",
-      stepDone: "Thank you! Someone will be in contact with you to help fill your need."
+      stepDone: "Thank you! Someone will soon be in contact with you to help."
     }
   },{
     language : "Spanish",
-    trigger : "NECESITAR",
+    trigger : "NECESIDAD",
     //triggers : ["NECESITAR", "NECESITO"],
     yes: ["SÍ", "SI"],
     messages : {
-      step1: "¿Qué necesitas?",
+      step1: "Gracias por contactar Harvey Necesidades. Lamentamos mucho la pérdida que ha experimentado. ¿Cuáles son sus necesidades inmediatas? Por favor sea especifico.",
       step2: handlebars.compile('¿Podemos ponernos en contacto con usted en ' +
         '{{ phone }}? Responda "{{ yes }}" o proporcione un número alternativo.'),
       step3: "¿Cuál es su código postal actual?",
-      stepDone: "¡Gracias! Alguien estará en contacto con usted para ayudar a llenar su necesidad."
+      stepDone: "¡Gracias! Alguien estará en contacto con ti pronto para ayudar."
     }
   }
 ]
